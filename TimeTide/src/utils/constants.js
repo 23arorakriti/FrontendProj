901813b1ee0a2d417cf7b401468c04c80a32ab60
23arorakriti <kriti23arora@gmail.com>
2025-06-26 12:49:31 +0530

    constants

---
+++ TimeTide/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const defaultCategories = [
+  { name: 'Work', color: '#4f46e5' },
+  { name: 'Sleep', color: '#22d3ee' },
+  { name: 'Chores', color: '#10b981' },
+  { name: 'Fun', color: '#f59e0b' },
+  { name: 'Scroll', color: '#ef4444' },
+  { name: 'Exercise', color: '#8b5cf6' },
+  { name: 'Study', color: '#3b82f6' },
+];
